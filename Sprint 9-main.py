f49dafc5d071b7cea81b6cc7458527dde94a7010
angelomp2001 <apassala@gmail.com>
2025-07-09 09:30:09 -0500

    update/ model r2 and RMSE.

--- Sprint 9-main.py
+++ Sprint 9-main.py
@@ -1,9 +1,34 @@
 import pandas as pd
-from data_explorers import view
-
+from data_explorers import view, see
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df_1 = pd.read_csv(r'C:\Users\Angelo\Documents\vscode\portfolio\portfolio-1\sprint 9 geo_data_0.csv')
 df_2 = pd.read_csv(r'C:\Users\Angelo\Documents\vscode\portfolio\portfolio-1\sprint 9 geo_data_0.csv')
 df_3 = pd.read_csv(r'C:\Users\Angelo\Documents\vscode\portfolio\portfolio-1\sprint 9 geo_data_0.csv')
 
-view([df_1, df_2, df_3], view='headers')
+view(df_1, view='headers')
+view(df_2, view='headers')
+view(df_3, view='headers')
+
+# we drop id as we don't need it for anything
+
+df_1 = df_1.drop(columns=['id'])
+df_2 = df_2.drop(columns=['id'])
+df_3 = df_3.drop(columns=['id'])
+
+
+see(df_1)
+# see(df_2)
+# see(df_3)
+
+# goal: max(oil wells value)
+# value: quality and volume
+# target: volume in new wells
+
+# region: max(total profit for the selected oil wells)
+# df = 1 region of samples
+
+df_1_training, df_1_validation = train_test_split(df_1, test_size=0.25, random_state=42)
+df_2_training, df_2_validation = train_test_split(df_2, test_size=0.25, random_state=42)
+df_3_training, df_3_validation = train_test_split(df_3, test_size=0.25, random_state=42)
---
+++ tests.py
@@ -0,0 +1,42 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+df_1 = pd.read_csv(r'C:\Users\Angelo\Documents\vscode\portfolio\portfolio-1\sprint 9 geo_data_0.csv')
+df_1 = df_1.drop(columns=['id'])
+# df_2 = df_2.drop(columns=['id'])
+# df_3 = df_3.drop(columns=['id'])
+
+
+
+# goal: max(oil wells value)
+# value: quality and volume
+# target: volume in new wells
+
+# region: max(total profit for the selected oil wells)
+# df = 1 region of samples
+
+target = df_1['product'].name
+features = df_1.drop(columns=target).columns
+
+df_1_training, df_1_validation = train_test_split(df_1, test_size=0.25, random_state=42)
+df_1_training_target = df_1_training[target]
+df_1_training_features = df_1_training[features]
+
+model = LinearRegression()
+model.fit(df_1_training_features, df_1_training_target)
+df_1_validation_target_actual = df_1_validation[target]
+scores = model.score(df_1_validation[features], df_1_validation_target_actual)
+print(f"Model score: {scores:.2f}")
+
+# Predict on the validation set using the correct features.
+predictions = model.predict(df_1_validation[features])
+
+# Compute the mean squared error between actual and predicted values.
+mse = mean_squared_error(df_1_validation[target], predictions)
+
+# RMSE is the square root of the MSE.
+rmse = np.sqrt(mse)
+print(f"RMSE: {rmse:.2f}")
