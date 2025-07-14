bc99807dd4c2d6710b05666098076e7210e2dda9
angelomp2001 <apassala@gmail.com>
2025-07-14 09:49:13 -0500

    debug/ fixed profit function.  swapped two lines of code, so now it filters by prediction, then maps prediction index to target.

--- Sprint 9-main.py
+++ Sprint 9-main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 df_1 = pd.read_csv(r'C:\Users\Angelo\Documents\vscode\portfolio\portfolio-1\sprint 9 geo_data_0.csv')
 df_2 = pd.read_csv(r'C:\Users\Angelo\Documents\vscode\portfolio\portfolio-1\sprint 9 geo_data_1.csv')
@@ -89,10 +90,14 @@ random_state = 42
 
 
 #defining functions:
-def profit(predictions, wells_to_select=WELLS_TO_SELECT):
-        top_200_wells = predictions.nlargest(wells_to_select)
-        #print(f"Top {top_200_wells} wells selected based on predictions.")
-        total_revenue = top_200_wells.sum() * REVENUE_PER_UNIT
+def profit(
+            target: pd.Series,
+            predictions: pd.Series,
+            wells_to_select: int = WELLS_TO_SELECT
+            ) -> float:
+        top_predictions = predictions.nlargest(wells_to_select)
+        target_of_predictions = target.loc[top_predictions.index]
+        total_revenue = target_of_predictions.to_numpy().sum() * REVENUE_PER_UNIT
         total_profit = total_revenue - BUDGET
         #print(f"Total profit from selected wells: {total_profit:.2f}")
         return total_profit
@@ -107,12 +112,14 @@ def product_predictions(
         '''
 
         model.fit(training[features], training[target])
-        predictions = model.predict(validation[features])
-        validation['predictions'] = predictions        
+        validation['predictions'] = model.predict(validation[features])     
         
-        return validation['predictions']
+        return validation
 
-def stats(predictions: pd.Series, wells_to_select=WELLS_TO_SELECT) -> pd.Series:
+def stats(
+            actual: pd.DataFrame,
+            predictions: pd.Series,
+            wells_to_select=WELLS_TO_SELECT) -> pd.Series:
     '''
         Calculate statistics for the predictions.
     '''
@@ -136,7 +143,7 @@ def stats(predictions: pd.Series, wells_to_select=WELLS_TO_SELECT) -> pd.Series:
     print(f'Model RMSE for {region}: {region_stats.loc[region, "RMSE"]:.2f}')
     MIN_VOL_PER_WELL = BUDGET / (REVENUE_PER_UNIT * WELLS_TO_SELECT)
     print(f"Minimum volume per well: {MIN_VOL_PER_WELL:.2f}")
-    region_stats.loc[region, 'Mean profit'] = np.mean(validation['predictions'])
+    region_stats.loc[region, 'Mean profit'] = profit(target=actual, predictions=predictions, wells_to_select=WELLS_TO_SELECT)
     print(f'Mean profit for {region}: {region_stats.loc[region, "Mean profit"]}')
     
     region_stats.loc[region, 'Standard deviation of profit'] = np.std(validation['predictions'])
@@ -152,10 +159,8 @@ def stats(predictions: pd.Series, wells_to_select=WELLS_TO_SELECT) -> pd.Series:
     })
 
 def bootstrap_predictions(
-    validation: pd.DataFrame,
-    model=None,
-    features: list = None,
-    samples: int = 1000,
+    dataframe: pd.DataFrame,
+    repeats: int = 1000,
     random_state: int = 42
     ):
     # Create a random number from a number
@@ -165,14 +170,12 @@ def bootstrap_predictions(
     region_profit_samples = {"region1": []}
     region = "region1"
 
-    for sample in range(samples):
+    for sample in range(repeats):
         # create a random number per sample
         sample_seed = rng.randint(0, 10000)
         # create a bootstrap sample of predictions from the validation set
-        bootstrap_sample = validation.sample(n=len(validation), replace=True, random_state=sample_seed)
-        preds = model.predict(bootstrap_sample[features])
-        bootstrap_sample['predictions'] = preds
-        region_profit_samples[region].append(profit(bootstrap_sample['predictions'], wells_to_select=WELLS_TO_SELECT))
+        bootstrap_sample = dataframe.sample(n=POINTS_STUDIED , replace=True, random_state=sample_seed)
+        region_profit_samples[region].append(profit(target=bootstrap_sample['product'], predictions=bootstrap_sample['predictions'], wells_to_select=WELLS_TO_SELECT))
         
     return region_profit_samples[region]
 
@@ -190,7 +193,7 @@ def bootstrap_stats(
         print(f'samples mean: {region_stats.loc[region, "Samples mean"]}')
         print(f'samples std: {region_stats.loc[region, "Samples std"]}')
         print(f'95% confidence interval for samples profit: {region_stats.loc[region, "95% confidence interval for samples profit"]}')
-        print(f'risk of loss: {region_stats.loc[region, "risk of loss"]}')
+        print(f'risk of loss: {region_stats.loc[region, "risk of loss"]:.2f}%')
 
 for region, df in dfs.items():
     print(f'Region: {region}')
@@ -200,110 +203,21 @@ for region, df in dfs.items():
     target = training['product'].name
     features = training.drop(columns=target).columns
 
-    predictions = product_predictions(training=training, validation=validation, model=model)
-    #  2.2. Train the model and make predictions for the validation set.
-    # define features and target
-    # target = df['product'].name
-    # features = df.drop(columns=target).columns
-    #see product_predictions)
+    # calculate predictions
+    validation_target_predictions  = product_predictions(training=training, validation=validation, model=model)
 
-    # define features and target for training and validation sets
-    # training_target = training[target]
-    # training_features = training[features]
-    #(see product_predictions)
-    # Train the model using Linear Regression.
-    # model = LinearRegression()
-    # model.fit(training_features, training_target)
-    #(see product_predictions)
-    #  2.3. Save the predictions and correct answers for the validation set.
-    # Predict on the validation set using the correct features.
-    # predictions = model.predict(validation[features])
-    # validation['predictions'] = predictions
-    #(see product_predictions)
+    # calculate stats
+    stats(actual=validation_target_predictions['product'], predictions=validation_target_predictions['predictions'], wells_to_select=WELLS_TO_SELECT)
 
-    # sorted_predictions = predictions.sort_values(ascending=False)
-    # top_200_wells = sorted_predictions.head(200)
-
-    stats(predictions=predictions, wells_to_select=WELLS_TO_SELECT)
-    #  2.4. Print the average volume of predicted reserves and model RMSE.
-    # Average volume of predicted reserves.
-    # average_volume = np.mean(predictions)
-    # region_stats.loc[region, 'mean predicted reserves'] = average_volume
-    # print(f'Average volume of predicted reserves for {region}: {average_volume:.2f}')
-    #(see stats)
-    # Compute the mean squared error between actual and predicted values.
-    # mse = mean_squared_error(validation[target], predictions)
-    # region_stats.loc[region, 'RMSE'] = np.sqrt(mse)
-    #(see stats)
-    # RMSE is the square root of the MSE.
-    # rmse = np.sqrt(mse)
-    # region_stats.loc[region, 'RMSE'] = rmse
-    # print(f'Model RMSE for {region}: {region_stats.loc[region, "RMSE"]:.2f}')
-    #(see stats)
-
-    #  2.5. Analyze the results.
-    # score = model.score(validation[features], validation[target])
-    # region_stats.loc[region, 'Model score'] = score
-    # print(f'Model score for {region}: {region_stats.loc[region, "Model score"]:.2f}')
-    #(see stats)
-    # this model is horrible.  We should bootstrap the predictions to get a better estimate of the model's performance.
-
-    # Prepare for profit calculation:
-    #  3.1. Store all key values for calculations in separate variables.
-    #(see product_predictions)
-    # BUDGET = 100_000_000  # $100 million
-    # WELLS_TO_SELECT = 200  # Top 200 wells
-    # REVENUE_PER_BARREL = 4.5  # $4.5 per barrel
-    # REVENUE_PER_UNIT = 4500  # $4,500 per thousand barrels
-    # POINTS_STUDIED = 500  # Study 500 points per region
-
-    #  3.2. Calculate the volume of reserves sufficient for developing a new well without losses. Compare the obtained value with the average volume of reserves in each region.
-    # MIN_VOL_PER_WELL = BUDGET / (REVENUE_PER_UNIT * WELLS_TO_SELECT)
-    #print(f"Minimum volume per well: {MIN_VOL_PER_WELL:.2f}")
-
-    #  3.3. Provide the findings about the preparation for profit calculation step.
-    # Write a function to calculate profit from a set of selected oil wells and model predictions:
-    #(see above)
-
-
-
-    #  4.1. Pick the wells with the highest values of predictions. 
-    # (see function)
-
-    #  4.2. Summarize the target volume of reserves in accordance with these predictions
-    # region_stats.loc[region, 'Mean profit'] = np.mean(validation['predictions'])
-    # print(f'Mean profit for {region}: {region_stats.loc[region, "Mean profit"]}')
-    
-    # region_stats.loc[region, 'Standard deviation of profit'] = np.std(validation['predictions'])
-    # print(f'Standard deviation of profit for {region}: {region_stats.loc[region, "Standard deviation of profit"]}')
-    
-    # region_stats.at[region, '95% confidence interval for profit'] = np.percentile(validation['predictions'], [2.5, 97.5]).tolist()
-    # print(f'95% confidence interval for profit for {region}: {region_stats.at[region, "95% confidence interval for profit"]}')
-
-    
-#  4.3. Provide findings: suggest a region for oil wells' development and justify the choice. Calculate the profit for the obtained volume of reserves.
-    # Calculate risks and profit for each region:
-    #      5.1. Use the bootstrapping technique with 1000 samples to find the distribution of profit.
-
-    top_200_total_profit = bootstrap_predictions(validation, model, features, samples=POINTS_STUDIED, random_state=random_state)
-
+    # calculate profit on predictions
+    top_200_total_profit = bootstrap_predictions(dataframe=validation_target_predictions, repeats=1000, random_state=random_state)
+    plt.hist(top_200_total_profit, bins=50, alpha=0.75)
+    plt.show()
+    # calculate bootstrap stats
     bootstrap_stats(top_200_total_profit, region, region_stats)
 
-    #      5.2. Find average profit, 95% confidence interval and risk of losses. Loss is negative profit, calculate it as a probability and then express as a percentage.
-    # region_stats.loc[region, 'Samples mean'] = np.mean(top_200_total_profit)
-    # region_stats.loc[region, 'Samples std'] = np.std(top_200_total_profit)
-    # region_stats.at[region, '95% confidence interval for samples profit'] = np.percentile(top_200_total_profit, [2.5, 97.5]).tolist()
-    # ci_lower = region_stats.loc[region, '95% confidence interval for samples profit'][0]
-    # ci_upper = region_stats.loc[region, '95% confidence interval for samples profit'][1]
-    # region_stats.loc[region, 'risk of loss'] = np.count_nonzero(np.array(top_200_total_profit) < 0) / len(top_200_total_profit) * 100
-    
-    # print(f'samples mean: {region_stats.loc[region, "Samples mean"]}')
-    # print(f'samples std: {region_stats.loc[region, "Samples std"]}')
-    # print(f'95% confidence interval for samples profit: {region_stats.loc[region, "95% confidence interval for samples profit"]}')
-    # print(f'risk of loss: {region_stats.loc[region, "risk of loss"]}')
 
-
-    #      5.3. Provide findings: suggest a region for development of oil wells and justify the choice.
+#      5.3. Provide findings: suggest a region for development of oil wells and justify the choice.
 print(region_stats)
 top_region = region_stats['Mean profit'].idxmax()
 print(f'Region with the highest mean profit: {region_stats.loc[top_region]}')
