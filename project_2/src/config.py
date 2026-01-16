f1a6b5f1d838319475273d0a0a92e13e943674bd
angelomp2001 <apassala@gmail.com>
2026-01-16 13:33:26 -0600

    update/ .ipynb file to run.  started progress.

--- project_2/src/config.py
+++ project_2/src/config.py
@@ -1,6 +1,6 @@
 # data path
-orders_path = '/content/instacart_orders.csv'
-products_path = '/content/products.csv'
-departments_path = '/content/departments.csv'
-aisles_path = '/content/aisles.csv'
-order_products_path = '/content/order_products.csv'
+orders_path = '/data/instacart_orders.csv'
+products_path = '/data/products.csv'
+departments_path = '/data/departments.csv'
+aisles_path = '/data/aisles.csv'
+order_products_path = '/data/order_products.csv'
