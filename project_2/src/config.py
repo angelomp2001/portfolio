ca955db0e58c57f5b993b39f6c44daf7bd060ec8
angelomp2001 <apassala@gmail.com>
2026-01-19 13:21:11 -0600

    update/ .ipynb works in antigravity now.

--- project_2/src/config.py
+++ project_2/src/config.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+
+# path to project root (assuming this file is in src/)
+project_root = Path(__file__).resolve().parent.parent
+data_dir = project_root / 'data'
+
 # data path
-orders_path = '/data/instacart_orders.csv'
-products_path = '/data/products.csv'
-departments_path = '/data/departments.csv'
-aisles_path = '/data/aisles.csv'
-order_products_path = '/data/order_products.csv'
+orders_path = data_dir / 'instacart_orders.csv'
+products_path = data_dir / 'products.csv'
+departments_path = data_dir / 'departments.csv'
+aisles_path = data_dir / 'aisles.csv'
+order_products_path = data_dir / 'order_products.csv'
